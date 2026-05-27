27270726ce1ff92a260a1f980a1cd8333a7584f8
Sajjad Ahmadi <sajjadahmadi4sa@gmail.com>
2026-05-27 15:49:17 +0330

    URL Routing
    Routing url for home , about pages

--- website/urls.py
+++ website/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from website.views import *
 
 urlpatterns = [
-    path('',index_view),
-    path('about/',about_view),
-    path('contact/',contact_view),
+    path('',index_view, name= 'Home'),
+    path('about/',about_view , name= 'About'),
+    path('contact/',contact_view, name= 'Contact'),
     
 ]
