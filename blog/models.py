1c3abfbecd193582809a0e253d8f4d8b4a7338d2
Sajjad Ahmadi <sajjadahmadi4sa@gmail.com>
2026-06-01 19:06:27 +0330

    models for blog and website
    create 2 models for apps

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Post (models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    counted_view = models.IntegerField(default= 0)
+    status = models.BooleanField(default= False)
+    published_date = models.DateField(null=True)
+    created_date = models.DateField(auto_now_add= True)
+    updated_date = models.DateField(auto_now=True)
+    
+    
 # Create your models here.
