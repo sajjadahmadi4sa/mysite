38ff9d56ec51c62cc3fd8413c960c7698f642f96
Sajjad Ahmadi <sajjadahmadi4sa@gmail.com>
2026-06-16 19:52:07 +0330

    post category
    created post category count sidebar

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
-from blog.models import Post
+from blog.models import Post , Category
+
 register = template.Library()
 
 @register.simple_tag(name = 'totalposts')
@@ -15,3 +16,12 @@ def snippet (value,arg=20):
 def latestposts ():
     posts = Post.objects.filter(status = 1).order_by('-published_date')[:4]
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories ():
+    posts = Post.objects.filter(status = 1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories :
+        cat_dict[name] = posts.filter(Category = name).count()
+    return {'categories':cat_dict}
