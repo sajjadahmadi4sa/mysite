fea5e29b8bac30dadbdd380aec652b723664d4f9
Sajjad Ahmadi <sajjadahmadi4sa@gmail.com>
2026-06-19 20:54:46 +0330

    latest posts
    train to  Template Tags

---
+++ website/templatetags/home_tags.py
@@ -0,0 +1,8 @@
+from django import template
+from blog.models import Post
+register = template.Library()
+
+@register.inclusion_tag('website/latest_posts.html')
+def latestposts():
+    posts = Post.objects.filter(status = 1).order_by('-published_date')[:6]
+    return{'posts':posts}
