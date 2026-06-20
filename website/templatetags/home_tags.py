af8099e84cc65698feb14fad7683f135ca227f36
Sajjad Ahmadi <sajjadahmadi4sa@gmail.com>
2026-06-20 16:51:40 +0330

    edit
    just a  small edit

--- website/templatetags/home_tags.py
+++ website/templatetags/home_tags.py
@@ -1,8 +1,21 @@
 from django import template
 from blog.models import Post
+from django.utils import timezone
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 register = template.Library()
 
-@register.inclusion_tag('website/latest_posts.html')
-def latestposts():
-    posts = Post.objects.filter(status = 1).order_by('-published_date')[:6]
+@register.inclusion_tag('website/latest_posts.html',takes_context=True)
+def latestposts(context):
+    request = context['request']
+    posts = Post.objects.filter(published_date__lte=timezone.now(),status=1).order_by('-published_date')[:6]
+    posts = Paginator(posts,3)
+    
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+    context = {'posts':posts}
     return{'posts':posts}
--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 def index_view(request):
     return render(request,'website/index.html')
